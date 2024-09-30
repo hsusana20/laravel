6d75d6308c488eefdeefdd81a23f6076bbc495ca
hsusana <152581281+hsusana20@users.noreply.github.com>
2024-09-30 08:06:08 +0000

    Added https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (App::environment(['staging', 'production'])) {
-            URL::forceScheme(' ');
+            URL::forceScheme('https');
         }
     
     }
